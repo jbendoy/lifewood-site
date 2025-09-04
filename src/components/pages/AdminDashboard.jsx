3ff07e097652cf005ad8d796a1c88f3442572286
jbdoy <jennifer.bendoy@cit.edu>
2025-09-04 20:25:07 +0800

    aray ko

--- src/components/pages/AdminDashboard.jsx
+++ src/components/pages/AdminDashboard.jsx
@@ -242,13 +242,17 @@ const AdminDashboard = () => {
                   <td>{app.lastName}</td>
                   <td>{app.email}</td>
                   <td>{app.project}</td>
-                  <td>
-                    {app.resume ? (
-                      <a href={`https://${API_BASE_URL}/uploads/${app.resume}`} target="_blank" rel="noreferrer">
-                        View Resume
-                      </a>
-                    ) : "N/A"}
-                  </td>
+<td>
+  {app.resume ? (
+    <a
+      href={`${API_BASE_URL}/uploads/${app.resume}`} // remove extra https:// if API_BASE_URL already has it
+      target="_blank"
+      rel="noreferrer"
+    >
+      View Resume
+    </a>
+  ) : "N/A"}
+</td>
                   <td>
                     <button className="btn btn-success" onClick={() => handleAccept(app._id)}>Accept</button>
                     <button className="btn btn-danger" onClick={() => handleDecline(app._id)}>Decline</button>
